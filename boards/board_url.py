00e9c77d0456dae2e43921e5660d345cf93967ce
elzaw <38485165+elzaw@users.noreply.github.com>
2022-04-16 19:30:46 +0200

    my first commit

---
+++ boards/board_url.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    path('', views.home,name="home"),
+    path('boards/<int:board_id>/',views.board_topics,name="board_topics"),
+    path('boards/<int:board_id>/new/',views.new_topic,name="new_topic"),
+    path('boards/<int:board_id>/topics/<int:topic_id>/',views.topic_posts,name="topic_posts")
+]
